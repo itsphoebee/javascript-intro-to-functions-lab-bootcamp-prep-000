538d43facd7ed3c97a4bfd969020c1136aa19a77
itsphoebee <phoebelau7@gmail.com>
2017-09-12 04:40:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ function logWhisper(string){
   console.log('hello')
 }
 function sayHiToGrandma(string){
-  if (string.toLowerCase === string)
-  return "I can\'t hear you!"
-  else if (string.toUpperCase === string)
-  return "YES INDEED!"
+  if (string.toLowerCase === string){
+  return "I can\'t hear you!"}
+  
+  else if (string.toUpperCase === string){
+  return "YES INDEED!"}
+  
   else {
-    return "I love you, too."
-  }
-
+    return "I love you, too."}
 }
