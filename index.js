843f9d6570fb89c88fa0fe1d2f48926aebe2e86f
itsphoebee <phoebelau7@gmail.com>
2017-09-12 04:36:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function sayHiToGrandma(string){
   if (string.toLowerCase === string)
   return "I can\'t hear you!"
   else if (string.toUpperCase === string)
-  return "YES INDEED!" 
+  return "YES INDEED!"
   else {
     return "I love you, too."
   }
-  
+
 }
